759fee6607839d62870dd82ab8ad9451db77d8cd
Alexey Kobiakov <alexey@kobiakov.eu>
2022-12-01 22:50:43 +0100

    Alright, that was easy

---
+++ src/nativeMain/kotlin/day01.kt
@@ -0,0 +1,26 @@
+fun main() {
+    // Part 1
+    solveDay(1) { input ->
+        readElvishLists(input)
+            .map(::toRation)
+            .maxOfOrNull(::totalCalories)
+    }
+    // Part 2
+    solveDay(1) { input ->
+        readElvishLists(input)
+            .map(::toRation)
+            .map(::totalCalories)
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
+}
+
+typealias ElvishList = List<String>
+fun readElvishLists(input: String): Sequence<ElvishList> = input.lines().partitionBy(String::isBlank).asSequence()
+
+typealias CalorieCount = Int
+typealias CarriedRation = List<CalorieCount>
+
+fun toRation(s: ElvishList): CarriedRation = s.map { it.toInt() }
+fun totalCalories(ration: CarriedRation): CalorieCount = ration.sum()
---
+++ src/nativeMain/kotlin/support.kt
@@ -0,0 +1,39 @@
+import io.ktor.client.*
+import io.ktor.client.engine.curl.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
+
+suspend fun fetchInput(day: Int): String {
+    val client = HttpClient(Curl)
+    val response: HttpResponse = client.get {
+        url("https://adventofcode.com/2022/day/$day/input")
+        header(
+            "cookie",
+            // NOTE: you'll need this token for the app to compile.
+            // Just create a file, say, token.kt, and make a const there.
+            "session=${AOC_TOKEN}"
+        )
+    }
+    if (response.status != HttpStatusCode.OK) {
+        throw RuntimeException("status != 200: $response")
+    }
+    return response.bodyAsText()
+}
+
+fun solveDay(dayNr: Int, solver: (String) -> Any?) {
+    runBlocking {
+        println("The answer is ${solver(fetchInput(dayNr))}")
+    }
+}
+
+fun <T> List<T>.partitionBy(predicate: (T) -> Boolean): List<List<T>> =
+    fold(mutableListOf(mutableListOf<T>())) { acc, t ->
+        if (predicate(t)) {
+            acc.add(mutableListOf())
+        } else {
+            acc.last().add(t)
+        }
+        acc
+    }.filterNot { it.isEmpty() }
